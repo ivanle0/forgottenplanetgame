aec532accd80e3d6d84f355363b2c567726a6c99
IvanLe0 <ivanleonardseo@yahoo.com>
2021-06-23 11:11:01 +0200

    tile width change

---
+++ game.js
@@ -0,0 +1,583 @@
+const mapDeck = [{
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-01.jpg",
+        beacon: "1y"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-02.jpg",
+        domeshelter: 1
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-03.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-04.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-05.jpg",
+        beacon: "2x"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-06.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-07.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front-base.jpg",
+        back: "images/maptile-08.jpg",
+        base: 1
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-09.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-10.jpg",
+        beacon: "3y"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-11.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-12.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-13.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-14.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-15.jpg",
+        beacon: "1x"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-16.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-17.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-18.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-19.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-20.jpg",
+        beacon: "2y"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-21.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-22.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-23.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-24.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-25.jpg",
+        domeshelter: 1
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-26.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-27.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-28.jpg",
+        beacon: "3x"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-29.jpg"
+    },
+    {
+        location: 0,
+        front: "images/maptile-front.jpg",
+        back: "images/maptile-30.jpg"
+    }
+];
+
+const eventDeck = [{
+        id: 1,
+        type: 1,
+        title: "Crater Shadow",
+        cardFace: "link",
+        effect: "Battery -1",
+        importance: 1
+    },
+    {
+        id: 2,
+        type: 1,
+        title: "Crater Shadow",
+        cardFace: "link",
+        effect: "Battery -1",
+        importance: 9
+    },
+    {
+        id: 3,
+        type: 1,
+        title: "Rough Terrain",
+        cardFace: "link",
+        effect: "Oxygen -2",
+        importance: 8
+    },
+    {
+        id: 4,
+        type: 1,
+        title: "Rough Terrain",
+        cardFace: "link",
+        effect: "Oxygen -1",
+        importance: 2
+    },
+    {
+        id: 5,
+        type: 1,
+        title: "Meteor Strike",
+        cardFace: "link",
+        effect: "Oxygen -1",
+        importance: 4
+    },
+    {
+        id: 6,
+        type: 1,
+        title: "Static Charge",
+        cardFace: "link",
+        effect: "Battery -1",
+        importance: 3
+    },
+    {
+        id: 7,
+        type: 1,
+        title: "Dust in Servomechs",
+        cardFace: "link",
+        effect: "Battery -1",
+        importance: 5
+    },
+    {
+        id: 8,
+        type: 1,
+        title: "Suit Overheating",
+        cardFace: "link",
+        effect: "Oxygen -2",
+        importance: 6
+    },
+    {
+        id: 9,
+        type: 1,
+        title: "Solar Wind",
+        cardFace: "link",
+        effect: "Battery -2",
+        importance: 7
+    },
+    {
+        id: 10,
+        type: 2,
+        title: "Solar Recharge",
+        cardFace: "link",
+        effect: "Battery +2"
+    },
+    {
+        id: 11,
+        type: 2,
+        title: "Solar Recharge",
+        cardFace: "link",
+        effect: "Battery +2"
+    },
+    {
+        id: 12,
+        type: 2,
+        title: "Solar Recharge",
+        cardFace: "link",
+        effect: "Battery +2"
+    },
+    {
+        id: 13,
+        type: 3,
+        title: "Jet Pack",
+        cardFace: "link",
+        effect: "Move to any explored tile. Battery -1"
+    },
+    {
+        id: 14,
+        type: 3,
+        title: "Jet Pack",
+        cardFace: "link",
+        effect: "Move to any explored tile. Battery -1"
+    },
+    {
+        id: 15,
+        type: 3,
+        title: "Jet Pack",
+        cardFace: "link",
+        effect: "Move to any explored tile. Battery -1"
+    },
+    {
+        id: 16,
+        type: 4,
+        title: "Satellite Uplink",
+        cardFace: "link",
+        effect: "View next 6 cards in deck"
+    }
+];
+
+const fuelTanks = [{
+        id: 1,
+        beaconx: 0,
+        beacony: 0,
+        location: 0
+    },
+    {
+        id: 2,
+        beaconx: 0,
+        beacony: 0,
+        location: 0
+    },
+    {
+        id: 3,
+        beaconx: 0,
+        beacony: 0,
+        location: 0
+    }
+];
+
+const player = {
+    id: 1,
+    location: 0,
+    battery: 9,
+    oxygen: 9
+};
+
+const discardDeck = [];
+let currentEvent = [];
+let currentEquipment = [];
+let playerEquipment = [];
+let turnCount = 0;
+
+const gridElement = document.querySelector(".map-grid");
+
+const playerToken = document.getElementById("player-token");
+
+function startGame() {
+    shuffle(mapDeck);
+    shuffle(eventDeck);
+    buildGameBoard();
+    findFuelTanks();
+    playerStartingLocation();
+    movementPhase();
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function buildGameBoard() {
+    let idx = 0;
+    for (let row = 1; row < 6; row++) {
+        for (let col = 1; col < 7; col++) {
+            let coord = row.toString() + col.toString();
+            mapDeck[idx].location = parseInt(coord);
+            let maptile = document.createElement("div");
+            maptile.classList.add("map-tile");
+            maptile.setAttribute("id", coord);
+            gridElement.appendChild(maptile);
+            let maptileinner = document.createElement("div");
+            maptileinner.classList.add("map-tile-inner");
+            maptile.appendChild(maptileinner);
+            let maptilefront = document.createElement("div");
+            maptilefront.classList.add("map-tile-front");
+            maptileinner.appendChild(maptilefront);
+            let img = document.createElement("img");
+            img.src = mapDeck[idx].front;
+            maptilefront.appendChild(img);
+            let maptileback = document.createElement("div");
+            maptileback.classList.add("map-tile-back");
+            maptileinner.appendChild(maptileback);
+            let img2 = document.createElement("img");
+            img2.src = mapDeck[idx].back;
+            maptileback.appendChild(img2);
+            idx++;
+        }
+    }
+}
+
+startGame();
+
+
+function findFuelTanks() {
+    //Note to self: should find a nicer way of doing this
+    //most likely a function that loops through an array of 1x 1y 2x 2y...etc
+    //forEach it finds the associated location and 
+    //creates a tank object based on that -- 3 tank objects
+    let beaconCards = mapDeck.filter(card => card.beacon);
+    let beacon1X = beaconCards.find(card => card.beacon === "1x").location;
+    let beacon1Y = beaconCards.find(card => card.beacon === "1y").location;
+    let beacon2X = beaconCards.find(card => card.beacon === "2x").location;
+    let beacon2Y = beaconCards.find(card => card.beacon === "2y").location;
+    let beacon3X = beaconCards.find(card => card.beacon === "3x").location;
+    let beacon3Y = beaconCards.find(card => card.beacon === "3y").location;
+    fuelTanks.find(tank => tank.id === 1).beaconx = beacon1X;
+    fuelTanks.find(tank => tank.id === 1).beacony = beacon1Y;
+    fuelTanks.find(tank => tank.id === 2).beaconx = beacon2X;
+    fuelTanks.find(tank => tank.id === 2).beacony = beacon2Y;
+    fuelTanks.find(tank => tank.id === 3).beaconx = beacon3X;
+    fuelTanks.find(tank => tank.id === 3).beacony = beacon3Y;
+
+    fuelTanks.find(tank => tank.id === 1).location = (beacon1X[0] + beacon1Y[1]);
+    fuelTanks.find(tank => tank.id === 2).location = (beacon2X[0] + beacon2Y[1]);
+    fuelTanks.find(tank => tank.id === 3).location = (beacon3X[0] + beacon3Y[1]);
+    console.log(fuelTanks);
+}
+
+function playerStartingLocation() {
+    player.location = mapDeck.find(card => card.base === 1).location;
+    let path = calculatePath(22, player.location);
+    movePlayer(player.location, path);
+}
+
+function movementPhase() {
+    //add click listener to actionMatchingCards button
+    
+    let possibleMoves = document.getElementsByClassName("map-tile");
+    for (var i = 0; i < possibleMoves.length; i++) {
+        possibleMoves[i].addEventListener('click', actionMove);
+    }
+  //remove click listener player.location map tile
+}
+
+function actionMove() {
+    let destination = this.id;
+    //remove click listener from matchingCardsAction button
+    //remove click listeners from all map tiles
+    let path = calculatePath(player.location, destination);
+    let movesNeeded = calculateMovesNeeded(path);
+    if (movesNeeded < 3) {
+        movePlayer(destination, path);
+    } else {
+        showMessageMovesNeeded(movesNeeded, destination, path);
+    }
+}
+
+
+function movePlayer(destination, path) {
+    if (path === undefined) {
+        path = calculatePath();
+    }
+    let [moveHorizontal, moveVertical] = path;
+    playerToken.style.transform += 'translateX(' + (moveHorizontal * 54) + 'px)';
+    setTimeout( function() {
+      playerToken.style.transform += 'translateY(' + (moveVertical * 54) + 'px)';
+    document.getElementById(destination).classList.add("map-tile-is-flipped");
+    player.location = destination;
+    console.log("player new location", player.location)
+    eventPhase();
+  },400);
+}
+
+function calculatePath(location, destination) {
+    let locy = location % 10;
+    let locx = (location - locy) / 10;
+    let desty = destination % 10;
+    let destx = (destination - desty) / 10;
+    let moveVertical = destx - locx;
+    let moveHorizontal = desty - locy;
+    console.log([moveHorizontal, moveVertical])
+    return [moveHorizontal, moveVertical];
+}
+
+function calculateMovesNeeded(path) {
+    let [moveHorizontal, moveVertical] = path;
+    //Math.abs ensures number is positive
+    let movesNeeded = Math.abs(moveHorizontal) + Math.abs(moveVertical);
+    return movesNeeded;
+}
+
+function showMessageMovesNeeded(movesNeeded, destination, path) {
+    showMessageSection();
+    //hide unnecessary buttons
+    let messageTitle = document.getElementById("message-title");
+    let messageText = document.getElementById("message-text");
+    let okButton = document.getElementById("message-ok-button");
+    let cancelButton = document.getElementById("message-cancel-button");
+    let oxygenButton = document.getElementById("message-oxygen-button");
+    let batteryButton = document.getElementById("message-battery-button");
+    cancelButton.addEventListener('click', movementPhase);
+    messageTitle.innerText = "Long Journey";
+    messageText.innerText = "That location is " + movesNeeded +
+        " moves away. This will cost you Oxygen or Battery.";
+    let counter = movesNeeded;
+    //another function needs to handle player lowering own oxygen or 
+    //battery values until counter is reduced to zero
+    //on OK trigger function payForTheMove(destination,movesNeeded,path)    
+}
+
+function showMessageSection() {
+    //unhide message section
+}
+
+function hideMessageSection() {
+    //hide message section
+}
+
+function eventPhase() {
+    dealEventCards();
+    if (currentEvent.length > 1) {
+        discardExcessEvents();
+    }
+    displayCurrentEvent();
+}
+
+function dealEventCards() {
+    for (i = 0; i < 3; i++) {
+        let container = eventDeck.shift();
+        if (container.type === 1) {
+            currentEvent.push(container);
+        } else {
+            currentEquipment.push(container);
+        }
+    }
+    if (eventDeck.length < 6) { //Must always have at least 6 cards in eventDeck...
+        replenishEventDeck(); //...to allow SatelliteUplink action
+    }
+}
+
+function replenishEventDeck() {
+    //shuffle discardDeck
+    eventDeck.push(...discardDeck);
+}
+
+function discardExcessEvents() {
+    currentEvent.sort(function(a, b) {
+        return a.importance - b.importance
+    });
+    while (currentEvent.length > 1) {
+        discardDeck.push(currentEvent.shift());
+    }
+}
+
+function displayCurrentEvent() {
+    showMessageSection();
+    //hide cancel button
+    let event = currentEvent[0];
+    let messageTitle = document.getElementById("message-title");
+    let messageText = document.getElementById("message-text");
+    let okButton = document.getElementById("message-ok-button");
+    okButton.addEventListener('click', displayCurrentEquipment);
+    messageTitle.innerText = "Event: " + event.title;
+    messageText.innerText = event.text;
+    event.effect; //do effects of event  
+}
+
+function displayCurrentEquipment() {
+    let messageTitle = document.getElementById("message-title");
+    let messageText = document.getElementById("message-text");
+    let okButton = document.getElementById("message-ok-button");
+    okButton.addEventListener('click', updatePlayerEquipment);
+    messageTitle.innerText = "You found new Equipment";
+    messageText.innerHTML = "";
+    for (i = 0; i < currentEquipment.length; i++) {
+        messageText.innerHTML += `<p>${currentEquipment[i].title}</p>`;
+    }
+}
+
+function updatePlayerEquipment() {
+    for (i = 0; i < currentEquipment.length; i++) {
+        playerEquipment.push(currentEquipment.shift());
+    }
+    matchingCardsCheck();
+}
+
+function matchingCardsCheck() {
+    //check if player has matching cards in playerEquipment
+    //if so allow player to use them
+    //else equipmentCheck()
+}
+
+function equipmentCheck() {
+    //check no of cards in player's hand
+    if (playerEquipment.length > 3) {
+        console.log("You can only have 3 cards in your hand at the end of a turn. " +
+            "You must discard " + (playerEquipment.length - 3) + " cards.");
+        //trigger function to allow equipment card discards
+    } else {
+        systemsCheck();
+    }
+}
+
+function systemsCheck() {
+    if (player.battery < 1 || player.oxygen < 1) {
+        console.log("You lost the game");
+        endGame();
+    } else {
+        endTurn()
+    }
+}
+
+function endTurn() {
+    turnCount++;
+    movementPhase();
+}
